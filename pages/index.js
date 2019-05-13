cd6f28a244ed4c02bfe49013a13d5d4760a7d350
DiegoHaramVazquezSolis <diegovazquezo@live.com.mx>
2019-05-12 21:47:29 -0500

    Recicled code from the past

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Index = () => {
+    return (
+        <>
+            <Head>
+                <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
+                <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500|Open+Sans:300,400" rel="stylesheet" />
+                <link href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" rel="stylesheet" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossOrigin="anonymous" />
+            </Head>
+        </>
+    );
+}
+
+export default Index;
