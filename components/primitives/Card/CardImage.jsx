e86350b7a0ca180d046062f10dc816b25a2dff2f
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2019-05-16 21:05:22 -0500

    Url added to source of CardImage

--- components/primitives/Card/CardImage.jsx
+++ components/primitives/Card/CardImage.jsx
@@ -6,7 +6,7 @@ const CardImage = ({ src }) => {
             <div></div>
             <style jsx>{`
                 div {
-                    background-image: ${src};
+                    background-image: url(${src});
                     height: 150px;
                     width: 100%;
                     border-radius: 4px 4px 0 0;
