353b08217d96c26a6d7afb2ed5fd4bf50a47fc6b
DiegoHaramVazquezSolis <diegovazquezo@live.com.mx>
2019-05-22 13:52:06 -0500

    Head added to layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,13 @@
+import Head from 'next/head';
+
 const Layout = ({ children }) => {
     return (
         <div className="page-layout">
+            <Head>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
+                <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500|Open+Sans:300,400" rel="stylesheet" />
+                <link href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" rel="stylesheet" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossOrigin="anonymous" />
+            </Head>
             {children}
             <style jsx global>{`
                 body {
