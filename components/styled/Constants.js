4e6059a68da24eda316c5f79f5e608921adb3d90
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2019-05-25 21:22:56 -0500

    Constants (with new colorimetry) added

---
+++ components/styled/Constants.js
@@ -0,0 +1,8 @@
+export const primaryColor = '#FDB963';
+export const primaryColorRGB = '253, 185, 99';
+export const primaryVariantColor = '#C78935';
+export const primaryVariantColorRGB = '199, 137, 53';
+export const secondaryColor = '#26C7DC';
+export const secondaryColorRGB = '38, 199, 220';
+export const secondaryVariantColor = '#0096AA';
+export const secondaryVariantColorRGB = '0, 150, 170';
