872e138e6f85db5c9a76e0808453a59eefc2cc5b
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2019-05-27 17:38:32 -0500

    DialogCartel view finished

--- components/composed/DialogCartel.jsx
+++ components/composed/DialogCartel.jsx
@@ -6,21 +6,20 @@ import Body1 from '../styled/Body1';
 import ButtonRaised from '../primitives/Buttons/ButtonRaised';
 import H4Styled from '../styled/H4Styled';
 import Map from '../primitives/Map/Map';
-import { primaryColor } from '../styled/Constants';
 import CheckBoxField from '../primitives/FormControls/CheckBoxField';
 import ButtonOutlined from '../primitives/Buttons/ButtonOutlined';
 
-const DialogCartel = () => (
+const DialogCartel = ({ name, date, specie, sex, place, description, haveId }) => (
     <>
         <Head>
-            <title>Kenji</title>
+            <title>{name}</title>
         </Head>
         <Dialog open>
             <div className='container mt-4'>
                 <div className='row'>
                     <div className='col-sm-12 col-md-4'></div>
                     <div className='col-sm-12 col-md-8'>
-                        <H4Styled>Kenji</H4Styled>
+                        <H4Styled>{name}</H4Styled>
                     </div>
                     <div className='col-sm-12 col-md-4 mb-4'>
                         <div
@@ -32,11 +31,11 @@ const DialogCartel = () => (
                                 height: '190px',
                                 backgroundSize: 'cover'
                             }}></div>
-                        <Body1 className='mb-2 text-center'>Visto por ultima vez el día: 26/05/2019</Body1>
+                        <Body1 className='mb-2 text-center'>Visto por ultima vez el día: {date}</Body1>
                         <Subtitle1 className='text-center' style={{ color: '#828282' }}>
-                            Especie: Perro <i style={{ color: primaryColor }} className='fas fa-dog fa-2x'></i><br />
-                            Sexo: Macho<br />
-                            Guadalajara, Jalisco, Mexico
+                            Especie: {specie}<br />
+                            Sexo: {sex}<br />
+                            {place}
                         </Subtitle1>
                         <div className='d-flex justify-content-center'>
                             <ButtonOutlined value='Compartir' className='mr-2' />
@@ -45,9 +44,9 @@ const DialogCartel = () => (
                     </div>
                     <div className='col-sm-12 col-md-8'>
                         <Subtitle1 style={{ color: '#4F4F4F' }}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin congue leo quis feugiat mattis. Aliquam vestibulum enim magna, vel ultricies lectus consectetur sit amet. Suspendisse faucibus metus molestie mi ullamcorper pretium. Vestibulum quis bibendum ipsum. Aenean eleifend sem vel eros blandit, a malesuada erat molestie. Maecenas ac cursus leo, a volutpat odio. Donec a dui sed justo placerat porta a a diam. Mauris sollicitudin dapibus lorem, ac iaculis turpis tempor quis. Donec lacinia libero neque, gravida aliquam quam rutrum ultricies. Fusce viverra pharetra iaculis. Ut leo velit, euismod ac dui a, sodales luctus nulla..
+                            {description}
                         </Subtitle1>
-                        <CheckBoxField className='mt-2 mb-0' disabled checked label='Tiene placa de identificación' />
+                        <CheckBoxField className='mt-2 mb-0' disabled checked={haveId} label='Tiene placa de identificación' />
                         <div className='ml-2'>
                             <Map label='Zona de la desaparición' draggable={false} initialPosition={{ latitude: 20.721870, longitude: -103.293555 }} />
                         </div>
