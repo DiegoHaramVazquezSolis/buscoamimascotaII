b7fb642bda12b1b0e954c3b5d6126012f9a6b10a
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2019-06-02 01:06:42 -0500

    Method to signIn with email and password created

--- firebase/auth.js
+++ firebase/auth.js
@@ -11,6 +11,16 @@ export async function createUserWithEmailAndPassword(email, password) {
 }
 
 /**
+ * Start session of a registred user
+ * @param {string} email email of the user
+ * @param {string} password password of the user
+ * @example signInWithEmailAndPassword(string_email.com, stringPassword)
+ */
+export async function signInWithEmailAndPassword(email, password) {
+    return await auth.signInWithEmailAndPassword(email, password);
+}
+
+/**
  * Update the user profile (profile based on FirebaseUser)
  * @param {object} data profile data
  * @example updateUserProfileData({ displayName: stringUserName })
