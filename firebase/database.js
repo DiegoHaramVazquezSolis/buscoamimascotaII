9abe047e8f8d2cd0ae826a1a07b9a2a121c4116b
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2019-06-02 20:04:02 -0500

    Create user on database when signIn with facebook method

--- firebase/database.js
+++ firebase/database.js
@@ -1,13 +1,27 @@
 import { db } from './firebase';
 
 /**
- * Create record for new user on database
+ * Create record for new user based on email on database
  * @param {FirebaseUser} user firebase user object
  * @example createUseRecord(user.user)
  */
-export function createUseRecord(user) {
+export function createUserRecordBasedOnEmail(user) {
     db.child('Users').child(user.uid).set({
         email: user.email,
         name: user.displayName
+    });
+}
+
+/**
+ * Create record for new user based on facebook on database
+ * @param {Object} user User object returned from facebook authentication
+ * @param {string} token FB token of the new user
+ */
+export function createUserRecordBasedOnFacebook(user, token) {
+    db.child('Users').child(user.uid).set({
+        email: user.email,
+        name: user.displayName,
+        photo: user.photoURL,
+        fbToken: token
     });
 }
