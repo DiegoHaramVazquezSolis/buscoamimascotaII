dacede749ff856262a32139876a1caa331c1762d
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2019-06-07 22:39:11 -0500

    Action adjusted to get data on real time

--- redux/actions/perdidasActions.js
+++ redux/actions/perdidasActions.js
@@ -2,7 +2,7 @@ import { db } from '../../firebase/firebase';
 import { GET_PERDIDAS } from './../ReduxConstants';
 
 export const loadPerdidas = () => async dispatch => {
-    await db.child('Perdidas').once('value', (mascotasPerdidas) => {
+    await db.child('Perdidas').on('value', (mascotasPerdidas) => {
         return dispatch({ type: GET_PERDIDAS , payload: mascotasPerdidas.val() });
     });
 }
