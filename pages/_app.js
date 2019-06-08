7891246a781076bb5e7d0347592f9ef9e802c63d
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2019-06-07 22:18:09 -0500

    Basic redux configuration added to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import App, { Container } from 'next/app';
 import Layout from '../components/Layout';
+import withRedux from 'next-redux-wrapper';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from '../redux/reducers/rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunkMiddleware from 'redux-thunk';
+
+const makeStore = (initialState, options) => {
+  return createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(thunkMiddleware)));
+};
 
 class BuscoAMiMascota extends App {
+  static async getInitialProps({Component, ctx}) {
+    
+    const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
+    return {pageProps};
+  }
+
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, store } = this.props;
     return (
       <Container>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <Provider store={store}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Provider>
       </Container>
     );
   }
 }
 
-export default BuscoAMiMascota;
+export default withRedux(makeStore)(BuscoAMiMascota);
