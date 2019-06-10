18d563f6f2d17c14f7ef05e6607bc171fa47dca9
DiegoHaramVazquezSolis <diegovazquezo@live.com.mx>
2019-06-09 20:31:16 -0500

    Publicar test created (need to be better)

---
+++ cypress/integration/Publicar.spec.js
@@ -0,0 +1,39 @@
+describe('Publicar test', function() {
+    beforeEach(() => {
+        cy.fixture('publicarData').as('publicar');
+    });
+    it('Get an element', function() {
+        cy.visit('/publicar');
+
+        cy.wait(1500);
+
+        cy.get('#name')
+            .type(this.publicar.newMascota.name)
+            .should('have.value', this.publicar.newMascota.name);
+        
+        cy.get('#specie')
+            .select(this.publicar.newMascota.specie)
+            .should('have.value', this.publicar.newMascota.specie);
+        
+        cy.get('#sex').check(this.publicar.newMascota.sex, {force: true});
+
+        cy.get('#description')
+            .type(this.publicar.newMascota.description)
+            .should('have.value', this.publicar.newMascota.description);
+        
+        cy.get('#lastSeen')
+            .type(this.publicar.newMascota.date)
+            .should('have.value', this.publicar.newMascota.date);
+        cy.get('#haveId').check({force: true});
+        
+        this.publicar.newMascota.contact.map((contact, index) => {
+            cy.get(`.fa-${contact.type}-${index}`).click();
+            cy.get(`#contact-${index}`)
+                .type(contact.content)
+                .should('have.value', contact.content);
+            if (this.publicar.newMascota.contact.length - 1 !== index) {
+                cy.contains('Agregar otro').click();
+            }
+        });
+    });
+});
